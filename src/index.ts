e735ad59786973ec0a4192104e3ab8dbc11e145d
Mick Hansen <maker@mhansen.io>
2022-11-30 16:22:09 +0100

    fix file reference

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export {default as PayloadViewer} from './components/PayloadViewer';
 export {default as HeaderViewer} from './components/HeaderViewer';
 export {default as EncodedViewer} from './components/EncodedViewer';
 export {default as JwtViewer} from './components/JwtViewer';
+
+export type {JwtHeader, JwtPayload} from './samples';
